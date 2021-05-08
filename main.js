c068ec777aed99765727d379ba36c4450f959453
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2021-05-08 12:09:54 -0300

    Almost end Itinerário Join

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 document.getElementById('classselect').value = localStorage.class || 'a'
 document.getElementById('LEselect').value = localStorage.estranclass || 'esp'
 
+if (localStorage.itinerarios) {
+    CheckSelectedBox()
+}
 async function StorageData(Content, KeytoSave, CheckBox, CheckId) {
 
     // Se eu quiser salvar uma data de um CheckBox (os itinerários..) ele entra...
@@ -29,9 +32,10 @@ async function StorageData(Content, KeytoSave, CheckBox, CheckId) {
 
 async function GetLink() {
     let Data = new Date();
+    let CurrentDay = Data.getDay
     let Time = Data.getHours() + (Data.getMinutes() / 100)
     if (Data.getDay === 0 || Data.getDay === 6) return ('É final de semana, vai dormir vagabundo !!');
-    if (Time < 7.25 || Time > 11.50) return ('Ainda é cedo ou já acabou a sua aula !!');
+    if (Time < 7.25 || Time > 11.50) return GetItinerarioOrAlert(time, CurrentDay)
     if (Time >= 9.05 && Time < 9.20) return ('Tá no recreio Vagabundo, vai fazer oque com o link ?!!');
     let SelectedClass = await document.getElementById('classselect').value;
     let SelectedEstranClass = await document.getElementById('LEselect').value;
@@ -65,7 +69,6 @@ function SaveArrayDatas(KeytoSave, Content, NewData) {
 
     if (NewData) {
         let Array = [Content]
-        console.log(Array)
         localStorage.setItem(KeytoSave, JSON.stringify(Array))
         return
     }
@@ -78,6 +81,26 @@ function SaveArrayDatas(KeytoSave, Content, NewData) {
 
 function RemoveArrayDatas(KeytoSave, Content) {
     let Array = JSON.parse(localStorage[KeytoSave])
-    Array.splice(Array.indexOf(Content))
+    Array.splice(Array.indexOf(Content), 1);
     localStorage.setItem(KeytoSave, JSON.stringify(Array))
+}
+
+function CheckSelectedBox() {
+    let Array = JSON.parse(localStorage.itinerarios)
+    Array.forEach(function(CheckBoxId) {
+
+        document.getElementById(CheckBoxId).checked = true;
+    })
+}
+
+function GetItinerarioOrAlert(time, day) {
+    if (!localStorage.itinerarios) return alert('Ainda é muito cedo ou já acabou sua aula!')
+    let AllClassJson = await fetch('./class.json')
+        .then(response => response.json())
+    let ItinerariosJson = AllClassJson[itinerarios]
+    let Array = JSON.parse(localStorage.itinerarios)
+    Array.forEach(function(ItineInscrito) {
+
+    })
+
 }
